0943c5d8d3c235c8c92d84b054577508de2efceb
Prasanna.Natarajan <prasanna@npras.in>
2016-12-22 20:42:20 +0530

    wip

--- lib/group/main.rb
+++ lib/group/main.rb
@@ -3,13 +3,12 @@ module Group
 
     DB = Sequel.connect('sqlite://group.db')
 
+
     def process!(args)
       Group.logger.info args
       @input_file = args.first
+      @output_file = "output/#{File.basename(@input_file)}"
       @matching_type = args.last
-      @format_row = false
-      input_basename = File.basename(@input_file)
-      @output_file = "output/#{input_basename}"
 
       create_tables
       parse_csv
@@ -22,44 +21,60 @@ module Group
       create_output_file do |out|
         headers = CSV.read(@input_file, headers: true).headers.unshift('Id')
         out << headers
-        @format_row = true if headers.include?('Email')
         CSV.foreach(@input_file, csv_opts) do |row|
-          process_row(row: row, out: out)
+          out << processed_row(row)
         end
       end
     end
 
-    def create_output_file
-      csv_out = CSV.open(@output_file, 'wb')
-      yield(csv_out)
-      csv_out.close
-    end
 
-
-    def process_row(row:, out:)
+    def processed_row(row)
       emails = [row[:email], row[:email1], row[:email2]].compact
       phones = [row[:phone], row[:phone1], row[:phone2]].compact
 
-      person_id = DB[:people].insert(
-        first_name: row[:firstname],
-        last_name: row[:lastname],
-        zip: row[:zip],
-      )
-      row[:id] = person_id
-      phones.each do |phone|
-        DB[:phones].insert(person_id: person_id, content: phone) rescue SQLite3::ConstraintException
+      case @matching_type
+      when 'same_email'
+        same_person_id = DB[:emails].select(:id).where(content: emails).order(:id).limit(1).first
+        if same_person_id
+          row[:id] = same_person_id[:id]
+        else
+          person_id = DB[:people].insert(
+            first_name: row[:firstname],
+            last_name: row[:lastname],
+            zip: row[:zip],
+          )
+          row[:id] = person_id
+          phones.each do |phone|
+            DB[:phones].insert(person_id: person_id, content: phone) rescue SQLite3::ConstraintException
+          end
+          emails.each do |email|
+            DB[:emails].insert(person_id: person_id, content: email) rescue SQLite3::ConstraintException
+          end
+        end
       end
-      emails.each do |email|
-        DB[:emails].insert(person_id: person_id, content: email) rescue SQLite3::ConstraintException
-      end
-      out << [:id, :firstname, :lastname, :phone1, :phone2, :email1, :email2, :zip].map { |r| row[r] }
+
+      [:id,
+       :firstname,
+       :lastname,
+       :phone1,
+       :phone2,
+       :email1,
+       :email2,
+       :zip].map { |r| row[r] }
     end
+
+
+    def create_output_file
+      CSV.open(@output_file, 'wb') { |f| yield(f) }
+    end
+
 
     def create_tables
       create_table_people
       create_table_phones
       create_table_emails
     end
+
 
     def create_table_people
       return if DB.table_exists?(:people)
@@ -71,6 +86,7 @@ module Group
       end
     end
 
+
     def create_table_phones
       return if DB.table_exists?(:phones)
       DB.create_table(:phones) do
@@ -81,6 +97,7 @@ module Group
       end
     end
 
+
     def create_table_emails
       return if DB.table_exists?(:emails)
       DB.create_table(:emails) do
@@ -90,6 +107,7 @@ module Group
         index :content, unique: true
       end
     end
+
 
   end
 end
